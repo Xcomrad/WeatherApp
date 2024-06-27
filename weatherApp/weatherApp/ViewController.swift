a24744ca7a07ef2b131cc69a48bbeb9e95677165
Xcomrad <butgus.dallas@gmail.com>
2024-06-27 09:38:56 +0300

    initialize project

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Владислав Бут-Гусаим on 27.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

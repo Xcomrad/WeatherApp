bb5aa214a907d7bece7efb83908a82202e17bd47
Xcomrad <butgus.dallas@gmail.com>
2024-07-01 11:46:50 +0300

    removed swithToFarengeit

--- weatherApp/weatherApp/Views/SettingsView.swift
+++ weatherApp/weatherApp/Views/SettingsView.swift
@@ -4,30 +4,6 @@ import UIKit
 final class SettingsView: UIView {
     
     var completionSwithTheme: (()->())?
-    var completionSwitchTemp: (()->())?
-    
-    lazy var unitLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Градусы"
-        label.textColor = UIColor.white
-        label.font = UIFont.boldSystemFont(ofSize: 18)
-        return label
-    }()
-    
-    lazy var segmentControlTemp: UISegmentedControl = {
-        let items = ["Цельсий", "Фаренгейт"]
-        let control = UISegmentedControl(items: items)
-        control.backgroundColor = .systemGray6
-        control.selectedSegmentIndex = 0
-        control.layer.cornerRadius = 8
-        control.layer.shadowColor = UIColor.white.cgColor
-        control.layer.shadowOffset = CGSize(width: 0, height: 2)
-        control.layer.shadowRadius = 4
-        control.layer.shadowOpacity = 0.3
-        control.layer.masksToBounds = false
-        control.addTarget(self, action: #selector(switchTemp), for: .touchUpInside)
-        return control
-    }()
     
     lazy var themeLabel: UILabel = {
         let label = UILabel()
@@ -63,11 +39,7 @@ final class SettingsView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Actions
-    @objc func switchTemp(_ sender: UISegmentedControl) {
-        completionSwitchTemp?()
-    }
-    
+    //MARK: - Action
     @objc func switchTheme(_ sender: UISegmentedControl) {
         completionSwithTheme?()
     }
@@ -82,26 +54,14 @@ extension SettingsView {
     }
     
     func setupViews() {
-        addSubview(unitLabel)
         addSubview(themeLabel)
-        addSubview(segmentControlTemp)
         addSubview(segmentControlTheme)
         
     }
     
     func setupConstraints() {
-        unitLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).inset(20)
-            make.leading.equalTo(self.snp.leading).inset(20)
-        }
-        
-        segmentControlTemp.snp.makeConstraints { make in
-            make.top.equalTo(unitLabel.snp.top).inset(30)
-            make.leading.trailing.equalTo(self).inset(20)
-        }
-        
         themeLabel.snp.makeConstraints { make in
-            make.top.equalTo(segmentControlTemp).inset(50)
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(20)
             make.leading.equalTo(self.snp.leading).inset(20)
         }
         
